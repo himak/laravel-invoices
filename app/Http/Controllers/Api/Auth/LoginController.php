90d688780fc7da6a60e2a754fd37596ffd2084f2
Marek Zofota <mzofota@gmail.com>
2023-09-07 13:35:34 +0200

    Create Api Login controller.

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  Request  $request
+     *
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::query()->where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $device    = substr($request->userAgent() ?? '', 0, 255);
+        $expiresAt = $request->remember ? null : now()->addMinutes(config('session.lifetime'));
+
+        return response()->json([
+            'access_token' => $user->createToken($device, expiresAt: $expiresAt)->plainTextToken,
+        ], status: Response::HTTP_CREATED);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('auth/register', \App\Http\Controllers\Api\Auth\RegisterController::class);
+Route::post('auth/login', \App\Http\Controllers\Api\Auth\LoginController::class);
